821b5962f73c8de8753a93c71192532645df5a47
Andre Holguin <andreesito@gmail.com>
2019-04-07 17:21:26 -0700

    added state and theme

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,14 @@ class FlutterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepOrangeAccent,
+      ),
       home: Scaffold(
           appBar: AppBar(title: Text('EasyList')),
-          body: ProductManager(),
+          body: ProductManager('Arepas'),
           ),
     );
   }
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 import'./products.dart';
 
 class ProductManager extends StatefulWidget {
+
+  final String startingProduct;
+  ProductManager(this.startingProduct);
+
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
   }
 }
-
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Arepas'];
 
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    
+    _products.add(widget.startingProduct);
+  }
   @override
   Widget build(BuildContext context) {
     return Column(children: [
       Container(
         margin: EdgeInsets.all(10.0),
         child: RaisedButton(
+          color: Theme.of(context).primaryColorLight,
           onPressed: () {
             setState(() {
               _products.add('Sopas');
